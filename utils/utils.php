0d63871a0a130584e3530bccc3fd869708449c60
Diana <diana_georgieva_136@abv.bg>
2020-06-06 22:09:38 +0300

    Change dir structure

---
+++ utils/utils.php
@@ -0,0 +1,20 @@
+<?php
+class Utils
+{
+    const STATUS = [
+        'Admin' => 'ADMIN',
+        'Permanent' => 'PERMANENT',
+        'Temporary' => 'TEMPORARY',
+        'Blocked' => 'BLOCKED'
+    ];
+
+    const DAYS_OF_WEEK = [
+        'Monday' => 'MONDAY',
+        'Tuesday' => 'TUESDAY',
+        'Wednesday' => 'WEDNESDAY',
+        'Thursday' => 'THURSDAY',
+        'Friday' => 'FRIDAY',
+        'Saturday' => 'SATURDAY',
+        'Sunday' => 'SUNDAY',
+    ];
+}
