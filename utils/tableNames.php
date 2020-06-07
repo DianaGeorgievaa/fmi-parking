c775b5ac0f55d07ff98dac12c6fe73e32f8177e3
Stoyan Yanev <syanev@vmware.com>
2020-06-07 11:01:13 +0300

    Add table names

---
+++ utils/tableNames.php
@@ -0,0 +1,10 @@
+<?php
+class TableNames
+{
+    const COURSES = "courses";
+    const PARKING_SPOT = "parking_spot";
+    const PROFILE_VIEWER = "profile_viewer";
+    const USERS = "users";
+    const USERS_COURSES = "users_courses";
+    const USER_PARKING_INFO = "user_parking_info";
+}
